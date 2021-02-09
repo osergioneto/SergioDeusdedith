c47ce0fdec1ef5dbd50a86409a6475a385a65826
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-02-09 15:28:24 -0300

    feat: add resolver to fetch teams

--- src/resolvers/teams.ts
+++ src/resolvers/teams.ts
@@ -1,9 +1,25 @@
+import { References, Result } from "generated/api";
+
 export default {
   Query: {
-    equipes: async (_ctx: any, { id }: any, { dataSources }: any) => {
-      const { resultados } = await dataSources.esportesAPI.getTeam(id);
-      console.log("resultados: ", resultados);
-      return resultados.status;
+    equipes: async (_ctx: any, { id }: any, { dataSources }: any): Promise<Result> => {
+      const { resultados, referencias, paginacao }: Result = await dataSources.esportesAPI.getTeam(id);
+      const parsedReferences = parseReferences(referencias);
+
+      return {
+        resultados,
+        referencias: parsedReferences,
+        paginacao
+      };
     }
   },
+}
+
+function parseReferences(references: any) {
+  const referenceKeys = Object.keys(references);
+  const referenceValues = referenceKeys.map((key) => Object.values(references[key]));
+
+  const parsedReferences = Object.fromEntries(referenceKeys.map((_, i) => [referenceKeys[i], referenceValues[i]]));
+
+  return parsedReferences;
 }
