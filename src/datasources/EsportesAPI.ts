12ce312b019d6212c9bc6ce20c275417dc2aff73
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-02-09 15:26:56 -0300

    chore: rename datasource folder

--- src/datasources/EsportesAPI.ts
+++ src/datasources/EsportesAPI.ts
@@ -3,7 +3,7 @@ import { RESTDataSource } from "apollo-datasource-rest";
 export default class EsportesAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'http://localhost:8080/';
+    this.baseURL = `http://localhost:8080/`;
   }
 
   async getTeam(id: number) {
