dbcfb280af90215e08eb92ec910a19fa63e45b09
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-02-16 15:35:20 -0300

    feat: group games by state

--- src/resolvers/teams.ts
+++ src/resolvers/teams.ts
@@ -1,4 +1,6 @@
-import { TeamResult, ChampionshipResult, GameResult } from "generated/api";
+import { TeamResult, ChampionshipResult, Game, GameResult } from "generated/api";
+import { parse, formatDistanceStrict, isAfter, differenceInMinutes } from "date-fns";
+// import { parse, formatDistanceStrictWithOptions } from "date-fns/fp";
 
 export default {
   Query: {
@@ -23,11 +25,12 @@ export default {
       };
     },
     games: async (_ctx: any, { date }: any, { dataSources }: any): Promise<GameResult> => {
-      const { resultados, referencias, paginacao }: GameResult = await dataSources.esportesAPI.getGames(date);
+      const { resultados, referencias, paginacao } = await dataSources.esportesAPI.getGames(date);
       const parsedReferences = parseReferences(referencias);
+      const jogos = groupGameByDate(resultados.jogos);
 
       return {
-        resultados,
+        resultados: { jogos },
         referencias: parsedReferences,
         paginacao
       };
@@ -42,4 +45,34 @@ export function parseReferences(references: any) {
   const parsedReferences = Object.fromEntries(referenceKeys.map((_, i) => [referenceKeys[i], referenceValues[i]]));
 
   return parsedReferences;
+}
+
+export function groupGameByDate(games: Game[]) {
+  const groupedGames = games.reduce<{ encerrado: Array<Game>, ao_vivo: Array<Game>, para_acontecer: Array<Game> }>((acc, curr) => {
+    const gameHour = parse(curr.hora_realizacao, "HH:mm:ss", new Date());
+    const minutes = differenceNowInMinutes(gameHour);
+
+    if (minutes <= -120) {
+      acc.encerrado.push(curr);
+    } else if (minutes > -120 && minutes <= 0) {
+      acc.ao_vivo.push(curr);
+    } else {
+      acc.para_acontecer.push(curr);
+    }
+
+    return acc;
+  }, {
+    encerrado: [],
+    ao_vivo: [],
+    para_acontecer: []
+  });
+
+  return groupedGames;
+}
+
+export function differenceNowInMinutes(gameHour: Date | number, hourNow: Date | number = Date.now()) {
+  let multiplier = 1;
+  if (isAfter(hourNow, gameHour)) { multiplier = -1; }
+
+  return Number(formatDistanceStrict(Date.now(), gameHour, { unit: "minute" }).replace(/\s.*/gi, "")) * multiplier;
 }
