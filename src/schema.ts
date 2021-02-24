1e95b3a1f4a5d836b8ed0cb6f622203ffe6eeb33
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-02-24 09:35:36 -0300

    chore: rename type folder

--- src/schema.ts
+++ src/schema.ts
@@ -4,7 +4,7 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import path from "path";
 
 const resolversArray = loadFilesSync(path.join(__dirname, "resolvers"), { recursive: true, extensions: ["js", "ts"] });
-const typesArray = loadFilesSync(path.join(__dirname, "types"), { recursive: true, extensions: ["gql"] });
+const typesArray = loadFilesSync(path.join(__dirname, "typedefs"), { recursive: true, extensions: ["gql"] });
 const resolvers = mergeResolvers(resolversArray);
 const typeDefs = mergeTypeDefs(typesArray);
 const schema = makeExecutableSchema({ typeDefs, resolvers });
